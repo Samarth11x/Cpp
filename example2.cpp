f16d7650440057e9070ec3ab177c1e431125f463
Samarth11x <samarthpatil3996@gmail.com>
2025-04-11 23:21:09 +0530

    Add todays task

--- example2.cpp
+++ example2.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main(){
     int a, b, sum;
     a = 10;
-    b = 20;
+    b = 20; 
     sum = a + b;
 
-    cout << "Sum : " << sum;
+    cout << "Sum : " << sum ;
     return 0;
 }
