220653a1e72b9d96aa80a8cb97bd7ff1bfc30546
Samarth11x <samarthpatil3996@gmail.com>
2025-04-17 23:56:51 +0530

    Cpp basics

---
+++ Array/example1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    std::string cars[] = {"BMW", "Toyata", "Mahindra", "Audi", "RR"};
+    std::cout<< cars[0] << " is my fav car \n";
+    std::cout<< cars[1] << " is good \n";
+    std::cout<< cars[2] << "'s build quality is good \n";
+    std::cout<< cars[3] << " is okey\n";
+    std::cout<< cars[4] << " is my dream car \n";
+
+    return 0;
+}
