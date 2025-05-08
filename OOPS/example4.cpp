4ddd87a26f827ceb8860caf18c9a86712a756e90
Samarth Patil <samarthpatil3996@gmail.com>
2025-05-09 00:11:29 +0530

    CPP basics

--- OOPS/example4.cpp
+++ OOPS/example4.cpp
@@ -4,9 +4,32 @@ using namespace std;
 
 class constructor{
 public:
-    string without_parameter ;
+    string name;
+    string branch;
+    int roll_no;
     
-    constructor(){
-        without_parameter = "I am a Constructor withoud parameter\n";
+    // constructor(){
+    //     branch = "BECS"; 
+    // }
+    //non-parameterised constructor;
+
+    constructor(string n, int r_n, string b){
+        name = n;
+        roll_no = r_n;
+        branch = b;
     }
+
+    void get_info(){
+        cout << "Name : "<< name << endl;
+        cout << "Roll No. : "<< roll_no << endl;
+        cout << "Branch : " << branch << endl;
+    }
+
 };
+
+int main(){
+    constructor s1("Sam", 140, "BECS");
+    s1.get_info();
+
+    return 0;
+}
